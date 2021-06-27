b6a62a4ee8b680e883881b649a6d695ed5eb01fe
Isaac Laquerre <isaac.l@videotron.ca>
2021-06-27 03:32:37 -0400

    Forgot I had to push lol

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const { getMaxListeners } = require("process");
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.videotron.ca",
+    port: 587,
+    auth: {
+        user: "isaac.l@videotron.ca",
+        pass: "josee10",
+    },
+});
+
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
+
+app.listen(
+    PORT,
+    () => console.log("App live and listening on port " + PORT)
+);
+
+var sess;
+
+app.get("/", (req, res) => {
+    return res.sendFile("index.html", { root: "public/views" });
+});
+
+app.get("/about", (req, res) => {
+    return res.sendFile("about.html", { root: "public/views" });
+});
+
+app.get("/projects", (req, res) => {
+    return res.sendFile("projects.html", { root: "public/views" });
+});
+
+app.get("/contact", (req, res) => {
+    return res.sendFile("contact.html", { root: "public/views" });
+});
+
+app.post("/contact", (req, res) => {
+    if (req.query.sendEmail == "" || req.query.sendEmail == true) {
+        const mail = {
+            from: req.body.email,
+            to: "isaac.l@videotron.ca",
+            subject: req.body.subject,
+            text: req.body.message,
+        };
+  
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+                return res.send({
+                    status: "error",
+                    error: "An internal server error occured",
+                    message: err
+                });
+            } else {
+                return res.send({
+                    status: "ok",
+                    success: true
+                });
+            }
+        });
+        return res.send({
+            status: "ok",
+            success: true
+        });
+    }else {
+        return res.send({
+            status: "error",
+            error: "An internal server error occured"
+        });
+    }
+});
+
+app.get("*", (req, res) => {
+    return res.status(404).sendFile("404.html", { root: "public/views" });
+});
---
+++ public/assets/js/default.js
@@ -0,0 +1,47 @@
+function handleSubmit(e) {
+    e.preventDefault();
+
+        const email = document.getElementById('email').value.trim();
+        const subject = document.getElementById('subject').value.trim();
+        const message = document.getElementById('message').value.trim();
+
+        const data = {
+            email,
+            subject,
+            message
+        };
+
+        fetch("/contact?sendEmail", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        }).then(body => body.json()).then(res => {
+            if (res.status === "ok")  {
+                alert("Successfully sent email!");
+            }else {
+                var errorDiv = document.createElement("DIV");
+                errorDiv.classList.add("center");
+                errorDiv.style.float = "right";
+                errorDiv.style.margin = "auto 30% auto 30%";
+                errorDiv.style.backgroundColor = "#ffcccc";
+                errorDiv.style.border = "2px solid #ff3333";
+                errorDiv.style.width = "50%;";
+                errorDiv.id = "error";
+                var errorSpan = document.createElement("SPAN");
+                errorSpan.classList.add("center");
+                errorSpan.style.margin = "50% 10px 10px 10px;";
+                errorSpan.style.width = "50%;";
+                errorSpan.style.color = "black";
+                errorSpan.innerHTML = res.error;
+                errorDiv.appendChild(errorSpan);
+                if (!document.getElementById("error")) document.querySelector("form").appendChild(errorDiv);
+                else {
+                    document.getElementById("error").innerHTML = res.error;
+                    document.getElementById("error").style.color = "black";
+                }
+            }
+        });
+}
