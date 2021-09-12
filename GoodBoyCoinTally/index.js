3175f5ec44e9a33818fb23523119e63a4fd5b6da
IsaacLaquerre <killers3d@gmail.com>
2021-09-12 13:50:15 -0400

    Hosted bot on website

--- GoodBoyCoinTally/index.js
+++ GoodBoyCoinTally/index.js
@@ -1,43 +1,9 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
 const Discord = require("discord.js");
+const config = require("./config.json");
+const utils = require("./utils.js");
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const mysql = require("mysql");
-const utils = require("./GoodBoyCoinTally/utils");
-const config = require("./GoodBoyCoinTally/config.json");
-
-var app = express();
-app.use(express.static(__dirname + '/public'));
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.set('views', __dirname + '/public/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-        user: process.env.EMAIL || undefined,
-        pass: process.env.PASSWORD || undefined,
-    },
-});
-
-transporter.verify(function(error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is ready to take our messages");
-    }
-});
-
-app.listen(
-    PORT,
-    () => console.log("App live and listening on port " + PORT)
-);
+var mysql = require('mysql');
 
 dbOptions = {};
 
@@ -63,6 +29,7 @@ var connection = mysql.createPool(dbOptions);
 
 const TOKEN = config.token;
 const goodBoyCoin = "<:goodboycoin:625181771335729173>";
+
 const commands = [{
         name: 'grant',
         description: 'Grants Good Boy coins',
@@ -135,72 +102,6 @@ const rest = new REST({ version: '9' }).setToken(TOKEN);
 })();
 
 var client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
-
-app.get("/", (req, res) => {
-    return res.sendFile("index.html", { root: "public/views" });
-});
-
-app.get("/about", (req, res) => {
-    if (req.query && req.query.getTally != undefined) {
-        utils.selectFromDB(connection, function(success, resp) {
-            if (success) {
-                return res.send({
-                    status: "ok",
-                    data: resp
-                });
-            } else {
-                return res.send({
-                    status: "error",
-                    error: resp
-                });
-            }
-        }, "users", "", "");
-    } else return res.sendFile("about.html", { root: "public/views" });
-});
-
-app.get("/projects", (req, res) => {
-    return res.sendFile("projects.html", { root: "public/views" });
-});
-
-app.get("/contact", (req, res) => {
-    return res.sendFile("contact.html", { root: "public/views" });
-});
-
-app.get("/epik", (req, res) => {
-    return res.sendFile("epik.html", { root: "public/views" });
-    //return res.sendFile("damedaepik.mp4", { root: "public/assets" });
-});
-
-app.post("/contact", (req, res) => {
-    if (req.query.sendEmail == "" || req.query.sendEmail == true) {
-
-        const mail = {
-            from: process.env.EMAIL,
-            to: process.env.EMAIL,
-            subject: req.body.subject,
-            text: req.body.message + "\n\nSent from: " + req.body.email,
-        };
-
-        transporter.sendMail(mail, (err, data) => {
-            if (err) {
-                return res.send({
-                    status: "error",
-                    error: "An internal server error occured",
-                    message: err
-                });
-            } else {
-                return res.send({
-                    status: "ok",
-                    success: true
-                });
-            }
-        });
-    }
-});
-
-app.get("*", (req, res) => {
-    return res.status(404).sendFile("404.html", { root: "public/views" });
-});
 
 client.on("ready", () => {
     console.info("Ready");
