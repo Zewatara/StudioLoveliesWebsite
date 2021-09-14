f4b85745fb083253ce0c3b78a4c2a35cbc5ad2af
IsaacLaquerre <killers3d@gmail.com>
2021-09-14 17:59:37 -0400

    now work yes?

--- index.js
+++ index.js
@@ -375,7 +375,7 @@ client.on('interactionCreate', async interaction => {
                                     var orderID = utils.generateId(8);
 
                                     if (parseInt(interaction.options.get("reward").value) === 6) {
-                                        if (client.guilds.fetch("842146071626514462").then(guild => guild.members.fetch(interaction.user.id).then(member => member.roles.cache.some(role => role.id === "852675470319026177")))) return interaction.reply("You already are part of the Children of Epik.");
+                                        if (client.guilds.fetch("842146071626514462").then(guild => guild.members.fetch(interaction.user.id).then(member => member.roles.cache.get("852675470319026177") ? true : false))) return interaction.reply("You already are part of the Children of Epik.");
                                         else {
                                             client.guilds.fetch("842146071626514462").then(guild => guild.members.fetch(interaction.user.id).then(member => member.roles.add("852675470319026177")));
                                             client.guilds.fetch("842146071626514462").then(guild => guild.channels.fetch("852675207290552321").then(channel => channel.send({
