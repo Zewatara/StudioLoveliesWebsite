309d6c4af08bf5039d9c7124186888a2040f5a7c
IsaacLaquerre <killers3d@gmail.com>
2021-09-14 18:54:31 -0400

    so close pls be done

--- GoodBoyCoinTally/utils.js
+++ GoodBoyCoinTally/utils.js
@@ -37,6 +37,8 @@ module.exports = {
         if (Array.isArray(value)) value = "('" + value.join("', '") + "')";
         else value = "('" + value + "')";
 
+        console.log("INSERT INTO " + table + row + " VALUES " + value + ";");
+
         connection.query("INSERT INTO " + table + row + " VALUES " + value + ";");
         callback();
     },
