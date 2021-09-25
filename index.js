e3099873333c2b1803095b41e68678c604706205
IsaacLaquerre <killers3d@gmail.com>
2021-09-25 12:54:36 -0400

    This should work

--- index.js
+++ index.js
@@ -289,8 +289,8 @@ client.on("ready", () => {
                 for (i in resp) {
                     if (parseInt(resp[i].miners) > 0) {
                         utils.updateRow(connection, "users", "minerAmount", (parseFloat(resp[i].minerAmount) + 0.01 * parseInt(resp[i].miners)), ["userID", resp[i].userID], function() {
+                            console.log(resp[i].username, resp[i].minerAmount, resp[i].coins);
                             if (Math.floor(parseFloat(resp[i].minerAmount)) >= 1) {
-                                console.log(resp[i].username, resp[i].minerAmount, resp[i].coins);
                                 utils.updateRow(connection, "users", "minerAmount", (parseFloat(resp[i].minerAmount) - Math.floor(parseFloat(resp[i].minerAmount))), ["userID", resp[i].userID], function() {
                                     utils.updateRow(connection, "users", "coins", (parseInt(resp[i].coins) + Math.floor(parseFloat(resp[i].minerAmount))), ["userID", resp[i].userID], function() {
                                         console.log(resp[i].username, resp[i].minerAmount, resp[i].coins);
