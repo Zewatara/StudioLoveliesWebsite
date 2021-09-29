23e37a684a8aa569082961bd54518bc8c3fa62bf
IsaacLaquerre <killers3d@gmail.com>
2021-09-28 22:29:06 -0400

    Console logging dms

--- index.js
+++ index.js
@@ -713,6 +713,9 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.on("messageCreate", (message) => {
+
+    if (message.channel.type === "DM") console.log(message.content);
+
     if (message.author.id != "375485987893149696") return;
 
     const args = message.content.split(" ").slice(1);
